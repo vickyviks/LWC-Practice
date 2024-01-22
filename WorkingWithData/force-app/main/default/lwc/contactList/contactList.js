d05a569e3e4e5a30b12f91539dc4fb3168879221
Vipul Pareta <vipulpareta4291@gmail.com>
2024-01-23 03:03:16 +0530

    LWC Trail - LDS handle Errors

--- WorkingWithData/force-app/main/default/lwc/contactList/contactList.js
+++ WorkingWithData/force-app/main/default/lwc/contactList/contactList.js
@@ -3,6 +3,7 @@ import FIRST_NAME from '@salesforce/schema/Contact.FirstName';
 import LAST_NAME from '@salesforce/schema/Contact.LastName';
 import EMAIL from '@salesforce/schema/Contact.Email';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
+import { reduceErrors } from 'c/ldsUtils';
 
 const COLUMNS = [
     { label: 'First Name', fieldName: FIRST_NAME.fieldApiName, type: 'text' },
@@ -13,4 +14,9 @@ export default class ContactList extends LightningElement {
     columns = COLUMNS;
     @wire(getContacts)
     contacts;
+
+    get errors(){
+        return (this.contacts.error)?
+        reduceErrors(this.contacts.error):[];
+    }
 }
